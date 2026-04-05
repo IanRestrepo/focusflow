0fa88289042bd8d88c4c87e840748d537dd51c1f
IanRestrepo <ianrpoecomco@gmail.com>
2026-04-05 18:42:45 -0500

    Disable CSRF check for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from '@astrojs/react';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  security: {
+    checkOrigin: false,
+  },
   integrations: [react()],
   vite: {
     ssr: {
